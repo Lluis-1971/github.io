c54658a0f81c0c562c497afd9b5b6e1cbac81271
Lluis-1971 <101072077+Lluis-1971@users.noreply.github.com>
2024-08-30 00:30:55 +0200

    Delete js directory

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-alert("Bienvenidos a mi sitio Web");
-var miNombre = "Luis Llorens Mompo";
-var correo = "luisllorensmompo@gmail.com";
-var telefono = "619 125 684"
-console.log("Mi nombre: "+miNombre);
-console.log("Mi correo electrónico: "+correo);
-console.log("Mi teléfono: "+telefono);
-alert("Mi nombre: "+miNombre+"\nMi correo electrónico: "+correo+"\nMi teléfono: "+telefono);
